4f2e4648536f28041e369dd2f2ef02365daa8177
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-01-20 01:36:26 -0300

    Commit 20/01 - Adição de suporte ao Redux devtools

--- src/frontend/store/storeConfig.js
+++ src/frontend/store/storeConfig.js
@@ -12,7 +12,7 @@ const reducers = combineReducers({
 })
 
 function storeConfig() {
-    return createStore(reducers)
+    return createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
 }
 
 export default storeConfig;
