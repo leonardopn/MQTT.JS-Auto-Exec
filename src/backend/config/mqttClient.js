13a529257717c5371b0bb460801431f0e7889d0f
Leonardo Petta do Nascimento <leonardocps9@protonmail.com>
2021-01-24 15:24:15 -0300

    adição de mensagens no log do terminal

--- src/backend/config/mqttClient.js
+++ src/backend/config/mqttClient.js
@@ -2,17 +2,27 @@ import mqtt from 'async-mqtt';
 import { io } from "../config/socketServer";
 
 function achaComandoEExecuta(comando) {
+    io.sockets.emit("LOG_TERMINAL", `Procurando o comando: "${comando}"`);
+    let commandExists = false;
     global.commands.forEach(command => {
         if ((command.name === comando || command.command === comando)) {
-            command.execCommand();
+            commandExists = true;
+            io.sockets.emit("LOG_TERMINAL", `O comando: "${comando}" foi localizado e executado.`);
+            command.execCommand().then(value => {
+                io.sockets.emit("LOG_TERMINAL", `Comando "${command.name}" foi finalizado e sua saída é: ${value.payload}`);
+            });
         }
     })
+
+    if (!commandExists) {
+        io.sockets.emit("LOG_TERMINAL", `Comando "${comando}" não foi encontrado.`);
+    }
 }
 
 function getMQTTConnection(params) {
     const client = mqtt.connect(params.host, { username: params.user, password: params.pass, keepalive: 5, reconnectPeriod: 1000, connectTimeout: 5000 });
     async function subscribeTopic() {
-        console.log("Conexão feita com sucesso!");
+        io.sockets.emit("LOG_TERMINAL", `Conexão ao servidor: ${params.host} feita com sucesso!`);
         try {
             client.subscribe("/devices/pc_gamer");
         } catch (e) {
@@ -21,29 +31,23 @@ function getMQTTConnection(params) {
     }
 
     async function errorConnect(error) {
-        console.log(error.message);
-        console.log("Deu erro");
+        io.sockets.emit("LOG_TERMINAL", "ERRO - " + error.message);
     }
 
     async function reconnect() {
-        console.log("Tentando Re-conexão");
+        io.sockets.emit("LOG_TERMINAL", `Tentando se re-conectar ao servidor MQTT no endereço: ${params.host}`);
     }
 
     async function offLine() {
-        console.log("Sem conexão");
-    }
-
-    async function close() {
-        console.log("Conexão fechada!");
+        io.sockets.emit("LOG_TERMINAL", `Conexão com o servidor MQTT foi perdida.`);
     }
 
     client.on("connect", subscribeTopic);
     client.on("error", errorConnect);
-    client.on("close", close);
     client.on("offline", offLine);
     client.on("reconnect", reconnect);
-    client.on('message', function (topic, message) {
-        console.log(message.toString());
+    client.on('message', function (_, message) {
+        io.sockets.emit("LOG_TERMINAL", `A mensagem: "${message.toString()}" foi recebida do servidor MQTT.`);
         achaComandoEExecuta(message.toString());
     });
 
